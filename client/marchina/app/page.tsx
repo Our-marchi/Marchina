520741a956b59c49df74cb5fbbfcabe7c89b0939
Wiizmed <louizmohamed44@gmail.com>
2024-08-07 17:29:09 +0100

    sigUp & logIn

--- client/marchina/app/page.tsx
+++ client/marchina/app/page.tsx
@@ -1,9 +1,11 @@
-import Login from '../Components/LogIn/page';
+import Login from '../app/LogIn/page'
+import SignUp from '../app/SinUp/page';
 
 export default function Home() {
   return (
     <main>
-      <Login />
+      {/* <Login /> */}
+      <SignUp />
     </main>
   );
 }
